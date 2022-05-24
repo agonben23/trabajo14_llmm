d1765fd9c1795bb51c07912232643f4bf8c24073
agonben23 <agonzalezb@iesrafaelalberti.es>
2022-05-24 21:42:44 +0200

    bucle creado

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,25 @@
 
 fun main() {
     val gestorNotas = GestorNotas("..\\trabajo14_llmm\\src\\main\\resources\\notas.xml")
-    gestorNotas.buscarNota("nota2")
+    var comando: List<String>
+    println("Bienvenido a Xpress Memories for Life")
+    do{
+        println("¿Qué deseas hacer?\n-r Mostrar todos las notas\n-r [id] Muestra la nota con la id elegida\n-exit Salir del programa\n")
+        comando = readln().split(" ")
+
+        when(comando[0]){
+            "-r" -> {
+                if (comando.size == 1) {
+                    gestorNotas.imprimirTodasLasNotas()
+                }else{
+                    gestorNotas.buscarNota(comando[1])
+                }
+            }
+
+        }
+
+    }while (comando[0] != "-exit")
+
+    println("Fin del programa")
 }
 
